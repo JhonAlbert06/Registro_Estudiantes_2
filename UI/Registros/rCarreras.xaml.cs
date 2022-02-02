5a3b3e4520606d123f5a2ba221693d0a8a817bde
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-02-02 08:02:38 -0400

    Update rCarreras.xaml.cs

--- UI/Registros/rCarreras.xaml.cs
+++ UI/Registros/rCarreras.xaml.cs
@@ -2,4 +2,10 @@ using System.Windows;
 using Tarea3_Registro_Estudiantes.Entidades;
 using Tarea3_Registro_Estudiantes.BLL;
 
-
+namespace Tarea3_Registro_Estudiantes.UI.Registros
+{
+    public partial class rCarreras : Window
+    {
+        
+    }
+}
