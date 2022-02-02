10c9f0863573459c21c05a98358bbb7b33139ccd
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-02-01 23:12:29 -0400

    Create Estudiantes.cs

---
+++ Entidades/Estudiantes.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tarea3_Registro_Estudiantes.Entidades
+{
+    public class Estudiantes
+    {
+        [Key]
+        public int EstudiantesID { get; set; }
+        public string? Nombres { get; set; }
+        public string? Email { get; set; }
+        public int CarreraID { get; set; }
+        public bool Activo { get; set; }
+        
+        /*
+        Estudiantes (int estudiantesid, string nombres, string email, int carrera, bool activo)
+        {
+            this.EstudiantesID = estudiantesid;
+            this.Nombres = nombres;
+            this.Email = email;
+            this.CarreraID = carrera;
+            this.Activo = activo;
+        }
+        */
+
+    }
+}
