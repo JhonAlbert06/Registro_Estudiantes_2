8c22fa705142ba17b5f2eee77a27400f3be25a0e
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-02-02 19:59:31 -0400

    Update rEstudiantes.xaml.cs

---
+++ UI/Registros/rEstudiantes.xaml.cs
@@ -0,0 +1,91 @@
+using System.Windows;
+using Tarea3_Registro_Estudiantes.Entidades;
+using Tarea3_Registro_Estudiantes.BLL;
+
+namespace Tarea3_Registro_Estudiantes.UI.Registros
+{
+    public partial class rEstudiantes : Window
+    {
+        private Estudiantes Estudiantes = new Estudiantes();
+
+        public rEstudiantes()
+        {
+            InitializeComponent();
+
+            this.DataContext = Estudiantes;
+        }
+
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = this.Estudiantes;
+        }
+
+        private void Limpiar()
+        {
+            this.Estudiantes = new Estudiantes();
+            this.DataContext = Estudiantes;
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+
+            if (string.IsNullOrWhiteSpace(Estudiantes.Nombre))
+            {
+                
+            }
+           
+            return esValido;
+        }
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var encontrado = EstudiantesBLL.Buscar(this.Estudiantes.EstudiantesID);
+
+            if (encontrado != null)
+            {
+                this.Estudiantes = encontrado;
+                Cargar();
+            }
+            else
+            {
+                Limpiar();
+                MessageBox.Show("No se encontro el libro!", "Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool paso = false;
+
+            if (!Validar())
+                return;
+
+            paso = EstudiantesBLL.Guardar(Estudiantes);
+
+            if (paso)
+                MessageBox.Show("Libro guardado con éxito", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+                MessageBox.Show("No se pudo guardar el libro", "Fallo", MessageBoxButton.OK, MessageBoxImage.Information);
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (EstudiantesBLL.Eliminar(Estudiantes.EstudiantesID))
+            {
+                Limpiar();
+                MessageBox.Show("Libro eliminado con éxito", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("No se pudo eliminar el libro", "Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
