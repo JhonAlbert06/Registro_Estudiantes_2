18c9980d71dc7a2e11dfe044679e1dded03f3a3e
Jhon Albert Guzman Rosario <79234293+JhonAlbert06@users.noreply.github.com>
2022-02-01 23:27:01 -0400

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,10 @@ namespace Tarea3_Registro_Estudiantes
     /// </summary>
     public partial class App : Application
     {
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs args)
+        {
+            MessageBox.Show($"Ocurrio un error:( {args.Exception.Message}","Error no controlado", MessageBoxButton.OK, MessageBoxImage.Error);
+            args.Handled = true;
+        }
     }
 }
